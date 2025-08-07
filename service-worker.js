6841bd4984ec432c518fed1f1ef71da8c449382f
wolfff2498 <palacioeman@gmail.com>
2025-08-07 13:51:49 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,23 @@
-
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('fuelcalc-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/icon-192.png',
-                '/icon-512.png'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open('fuelcalc-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/service-worker.js',
+        '/icon-192.png',
+        '/icon-512.png'
+        // Add other assets here if needed (e.g., CSS, JS)
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
